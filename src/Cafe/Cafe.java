b949de42dff492cd9fa3c6c068c0377e75ac50ff
Maria <m.glovatskaya@godeltech.com>
2020-04-14 11:27:24 +0300

    my first commit

---
+++ src/Cafe/Cafe.java
@@ -0,0 +1,56 @@
+package Cafe;
+
+import java.util.Scanner;
+
+public class Cafe {
+    private String[][] menu = {{"Espresso", "Latte", "Capuchino"}, {"Tosts", "Omelette", "Cheesecakes"}};
+    private double[][] prise = {{2.50, 3.10, 4.20}, {3.40, 4.10, 5.00}};
+    private double myMoney = 15.70;
+    private double iPay;
+
+    private void Info(String message) {
+        System.err.println(message);
+    }
+
+    public Cafe() {
+        System.out.println("Good morning. Please see our menu:");
+        for (int i = 0; i < menu.length; i++) {
+            for (int x = 0; x < menu[i].length; x++)
+                System.out.println(menu[i][x] + " Price " + prise[i][x]);
+        }
+    }
+
+    protected void takeOrder() {
+        Info("Are you ready to make an order?");
+        Scanner scanner = new Scanner(System.in);
+        String answer = scanner.next();
+        if (answer.equalsIgnoreCase("Yes")) {
+            Info("What would you like to order?");
+            String inputString = scanner.next();
+                Info("Your order is:" + isItemInMenu(inputString));
+        } else{
+            Info("Sure, I'll come back to you a bit later");
+        }
+
+    }
+private String isItemInMenu(String item){
+    for (int i = 0; i < menu.length; i++) {
+        for (int x = 0; x < menu[i].length; x++)
+            if(menu[i][x].equalsIgnoreCase(item) ){
+                iPay = iPay + prise[i][x];
+                return "Your order is:" + menu[i][x] + "with prise:" + prise[i][x];
+            }else{
+                System.out.println("There is no such position in menu");
+                return "";
+            }
+    }
+    return "";
+}
+
+protected void pay(){
+    myMoney = myMoney - iPay;
+Info("Money paid: " + iPay + "Money left: " + myMoney);
+}
+
+}
+//грокаем Алгоритмы
---
+++ src/Cafe/Customer.java
@@ -0,0 +1,10 @@
+package Cafe;
+
+public class Customer {
+    public static void main(String[] args) {
+        Cafe Customer = new Cafe();
+        Customer.takeOrder();
+        Customer.pay();
+    }
+
+}
